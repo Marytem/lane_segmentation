5801a8486053b6804024c4e976557eb4627696ac
marytem <maryanatem1412@gmail.com>
2020-02-11 15:56:16 +0200

    modify tusimple processing

--- data_processing/process_tusimple.py
+++ data_processing/process_tusimple.py
@@ -4,38 +4,48 @@ import json
 from shutil import copyfile
 import random
 
-
+#  fix dividing
 random.seed(230)
 
-structure_path = 'data/tusimple/segm_structure/'
-data_path = 'data/tusimple/train_set/'
-labels = ['0601', '0531', '0313']
-clips = []
+structure_path = '../data/'
+train_data_path = '../unprocessed_data/TuSimple/train_set/'
+test_data_path = '../unprocessed_data/TuSimple/test_set/'
+train_labels = ['0601', '0531', '0313']
+train_clips = []
 ratio = 0.1
 
-#
-for label in labels:
-    labe_filename = data_path + 'label_data_%s.json' % label
-    clips += [json.loads(line) for line in open(labe_filename).readlines()]
-random.shuffle(clips)
+for label in train_labels:
+    labe_filename = train_data_path + 'label_data_%s.json' % label
+    train_clips += [json.loads(line) for line in open(labe_filename).readlines()]
+random.shuffle(train_clips)
 
-for clip, i in zip(clips, range(len(clips))):
-    if i % 100 == 0:
-        print(i)
-    lanes = clip['lanes']
-    filepath = clip['raw_file']
-    ysamples = clip['h_samples']
-    mode = 'train'
-    if i / len(clips) > 1 - ratio:
-        mode = 'val'
-    lanes = [[(x, y) for (x, y) in zip(lane, ysamples) if x >= 0] for lane in lanes]
+test_labe_filename = '../unprocessed_data/TuSimple/test_label.json'
+test_clips = [json.loads(line) for line in open(test_labe_filename).readlines()]
 
-    copyfile(data_path + filepath,
-             '{}{}_fr/{}/{}_frame_{}.jpg'.format(structure_path, mode, mode, mode, i))
+for data_path, clips in zip((test_data_path, train_data_path), (test_clips, train_clips)):
+    for clip, i in zip(clips, range(3, len(test_clips))):
 
-    label_image = np.zeros(raw_image.shape[:2], dtype=np.uint8)
-    for lane in lanes:
-        cv2.polylines(label_image, np.int32([lane]), isClosed=False, color=(255, 255, 255),
-                      thickness=5)
-    cv2.imwrite('{}{}_masks/{}/{}_mask_{}.png'.format(structure_path, mode, mode, mode, i),
-                label_image)
+        # progress
+        if i % 100 == 0:
+            print(i)
+
+        lanes = clip['lanes']
+        filepath = clip['raw_file']
+        ysamples = clip['h_samples']
+        mode = 'train'
+        if i / len(clips) > 1 - ratio:
+            mode = 'val'
+
+        copyfile(data_path + filepath,
+                 '{}{}_fr/{}/{}_frame_{}.jpg'.format(structure_path, mode, mode, mode, i))
+
+        lanes = [[(x, y) for (x, y) in zip(lane, ysamples) if x >= 0] for lane in lanes]
+        raw_image = cv2.imread(data_path + filepath)
+        label_image = np.zeros(raw_image.shape[:2], dtype=np.uint8)
+        for lane in lanes:
+            cv2.polylines(label_image, np.int32([lane]), isClosed=False, color=(255, 255, 255),
+                          thickness=5)
+        cv2.imwrite('{}{}_masks/{}/{}_mask_{}.png'.format(structure_path, mode, mode, mode, i),
+                    label_image)
+
+    break
